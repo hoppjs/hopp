c6fec153e1aa6e147de81b2b7e20e97aec787c3d
Karim Alibhai <karim@alibhai.co>
2017-06-13 21:47:24 -0400

    Decouple node legacy support from stable

--- index.js
+++ index.js
@@ -6,5 +6,11 @@
  * @copyright 2017 Karim Alibhai.
  */
 
-require('regenerator-runtime/runtime')
-require('./dist')
+const NODE_VERSION = +process.version.substr(1).split('.')[0]
+
+if ( NODE_VERSION < 7 ) {
+  require('regenerator-runtime/runtime')
+  require('./dist-legacy')
+} else {
+  require('./dist')
+}
