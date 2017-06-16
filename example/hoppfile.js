f4fc515d4d3c6ae084acb8e6f7d343a354396f42
Karim Alibhai <karim@alibhai.co>
2017-06-16 18:29:04 -0400

    Fix example hoppfile destinations

--- example/hoppfile.js
+++ example/hoppfile.js
@@ -2,11 +2,12 @@ import hopp from 'hopp'
 
 export const css =
   hopp([ 'src/css/*.css' ])
-    .dest('dist')
+    .dest('dist/css')
 
 export const js =
-  hopp([ 'src/js/*.js' ])
-    .dest('dist')
+  hopp([ 'src/js/*.js' ]) // create fs streams
+    // .babel()              // pipe to (create babel stream)
+    .dest('dist/js')         // pipe to (create dest stream)
 
 export default [
   'js',
