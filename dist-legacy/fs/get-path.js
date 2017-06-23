f2c23e947c7c9439592a62c586d7b4c2f9f71ca7
Karim Alibhai <karim@alibhai.co>
2017-06-23 15:20:09 -0400

    Fix sourcemapping

--- dist-legacy/fs/get-path.js
+++ dist-legacy/fs/get-path.js
@@ -13,4 +13,4 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = function (url) {
   return url[0] !== '.' && url[0] !== '/' ? './' + url : url;
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9mcy9nZXQtcGF0aC5qcyJdLCJuYW1lcyI6WyJ1cmwiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7a0JBTWU7QUFBQSxTQUFPQSxJQUFJLENBQUosTUFBVyxHQUFYLElBQWtCQSxJQUFJLENBQUosTUFBVyxHQUE3QixHQUFtQyxPQUFPQSxHQUExQyxHQUFnREEsR0FBdkQ7QUFBQSxDIiwiZmlsZSI6ImdldC1wYXRoLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBAZmlsZSBzcmMvZ2V0LXBhdGguanNcbiAqIEBsaWNlbnNlIE1JVFxuICogQGNvcHlyaWdodCAyMDE3IDEwMjQ0ODcyIENhbmFkYSBJbmMuXG4gKi9cblxuZXhwb3J0IGRlZmF1bHQgdXJsID0+IHVybFswXSAhPT0gJy4nICYmIHVybFswXSAhPT0gJy8nID8gJy4vJyArIHVybCA6IHVybFxuIl19
+//# sourceMappingURL=get-path.js.map
