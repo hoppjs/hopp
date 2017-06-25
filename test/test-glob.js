add4926964559613e8aef14d3905683ad8c185f8
Karim Alibhai <karim@alibhai.co>
2017-06-25 16:34:45 -0400

    Add glob unit tests

--- test/test-glob.js
+++ test/test-glob.js
@@ -8,12 +8,47 @@ const tmp = require('tmp');
 const glob = require('../packages/hopp/dist/fs/glob').default
 const cache = require('../packages/hopp/dist/cache')
 
-describe('glob', () => {
-  it('*.js', async () => {
+describe('glob', async () => {
+  it('should recursively match files relative to current dir', async () => {
     // load cache from new dir, shouldn't have lockfile
     await cache.load(tmp.dirSync().name)
 
-    // tests
+    // tests with no .; these snapshots are assumed to be correct
+    // in later tests
     expect(await glob('*.js', `${__dirname}/fixtures/glob`, false, true)).toMatchSnapshot()
+    expect(await glob('*/*.js', `${__dirname}/fixtures/glob`, false, true)).toMatchSnapshot()
+    expect(await glob('**/*.js', `${__dirname}/fixtures/glob`, false, true)).toMatchSnapshot()
+  })
+
+  it('should recursively match files relative to ./', async () => {
+    // load cache from new dir, shouldn't have lockfile
+    await cache.load(tmp.dirSync().name)
+
+    async function assert (pttn) {
+      const relativeToDot = await glob('./' + pttn, `${__dirname}/fixtures/glob`, false, true)
+      const relativeToCurr = await glob(pttn, `${__dirname}/fixtures/glob`, false, true)
+
+      return expect(relativeToDot).toEqual(relativeToCurr)
+    }
+
+    await assert('*.js')
+    await assert('*/*.js')
+    await assert('**/*.js')
+  })
+
+  it('should recursively match files relative to ../', async () => {
+    // load cache from new dir, shouldn't have lockfile
+    await cache.load(tmp.dirSync().name)
+
+    async function assert (pttn) {
+      const relativeToDot = await glob('../' + pttn, `${__dirname}/fixtures/glob/1`, false, true)
+      const relativeToCurr = await glob(pttn, `${__dirname}/fixtures/glob`, false, true)
+
+      return expect(relativeToDot).toEqual(relativeToCurr)
+    }
+
+    await assert('*.js')
+    await assert('*/*.js')
+    await assert('**/*.js')
   })
 })
