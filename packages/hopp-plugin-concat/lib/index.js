bc446a38832eddd2aebdde05a734e1b92e310c07
Riotpiaole <rlian072@uottawa.ca>
2017-07-08 22:16:20 -0400

    Added prefix and suffix for bundle

--- packages/hopp-plugin-concat/lib/index.js
+++ packages/hopp-plugin-concat/lib/index.js
@@ -18,7 +18,35 @@ export const config = {
 
 /**
  * We don't need to do any real transformation.
+ * 
+ * suffix and prefix features
+ * @param suffix || null @param prefix || null
+ * appending additional suffix and prefix to each data.
  */
 export default async (ctx, data) => {
+  /**
+   * unexpected num of arguments handling
+   */
+  if (ctx.args.length > 1) {
+    throw new Error('Unexpected numbers of arguments.')
+  }
+
+  /**
+   * options assign
+   */
+  var opts = Object.assign({}, ctx.args[0] || {})
+
+  // suffix 
+  if (opts.suffix) {
+    data.size += opts.suffix.length
+    data.body = Buffer.from(opts.suffix + data.body.toString())
+  }
+
+  // prefix
+  if (opts.prefix) {
+    data.size += opts.prefix.length
+    data.body = Buffer.from(data.body.toString() + opts.prefix)
+  }
+
   return data
 }
